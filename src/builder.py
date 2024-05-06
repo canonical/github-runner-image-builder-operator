35773c161f8dbd2c4767334097e71d79994c3527
charlie4284 <yangsoo.yoon@canonical.com>
2024-05-06 22:47:40 +0800

    feat app branch

--- src/builder.py
+++ src/builder.py
@@ -121,7 +121,7 @@ def _install_dependencies() -> None:
             [
                 "/usr/bin/pipx",
                 "install",
-                "git+https://github.com/canonical/github-runner-image-builder",
+                "git+https://github.com/canonical/github-runner-image-builder@feat/app",
             ],
             timeout=5 * 60,
             check=True,
