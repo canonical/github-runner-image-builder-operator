cef214d21b46b2906f5ee6cc87b33f58e15eb618
charlie4284 <yangsoo.yoon@canonical.com>
2024-05-07 12:30:52 +0800

    correct contents

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -141,7 +141,7 @@ def openstack_connection_fixture(clouds_yaml_contents: str) -> Connection:
     """The openstack connection instance."""
     clouds_yaml = yaml.safe_load(clouds_yaml_contents)
     clouds_yaml_path = Path.cwd() / "clouds.yaml"
-    clouds_yaml_path.write_text(data=clouds_yaml, encoding="utf-8")
+    clouds_yaml_path.write_text(data=clouds_yaml_contents, encoding="utf-8")
     first_cloud = next(iter(clouds_yaml["clouds"].keys()))
     return openstack.connect(first_cloud)
 
