6f99a900f7d2adf3834c7c5d1672b46f60dbbfe5
charlie4284 <yangsoo.yoon@canonical.com>
2024-07-02 15:17:44 +0800

    test: fix no await

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -110,7 +110,7 @@ async def test_charm_fixture(model: Model, test_id: str) -> AsyncGenerator[Appli
     subprocess.check_call(build_cmd)
     logger.info("Deploying built test charm.")
     app_name = f"test-{test_id}"
-    await juju_cli_deploy(f"./test_ubuntu-22.04-{get_juju_arch()}.charm", name=app_name)
+    juju_cli_deploy(f"./test_ubuntu-22.04-{get_juju_arch()}.charm", name=app_name)
     app = Application(entity_id=app_name, model=model, connected=False)
 
     yield app
