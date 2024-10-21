88390757fddbf9b982747e4f551f36b33853fd14
Yanks Yoon <yangsoo.yoon@canonical.com>
2024-10-21 10:13:06 +0000

    add COS agent provider

---
+++ src/cos.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Observer module for Jenkins to COS integration."""
+
+
+import ops
+from charms.grafana_agent.v0.cos_agent import COSAgentProvider
+
+
+class Observer(ops.Object):
+    """The Jenkins COS integration observer."""
+
+    def __init__(self, charm: ops.CharmBase):
+        """Initialize the observer and register event handlers.
+
+        Args:
+            charm: The parent charm to attach the observer to.
+        """
+        super().__init__(charm, "cos-observer")
+        self._grafana_agent = COSAgentProvider(self)
