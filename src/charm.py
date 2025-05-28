4879df7cb8871505b1e97df043dbef5985b320c5
Christopher Bartz <christopher.bartz@canonical.com>
2025-05-28 08:09:49 +0200

    create common fct to create configs

--- src/charm.py
+++ src/charm.py
@@ -8,6 +8,7 @@ import json
 import logging
 import time
 import typing
+from dataclasses import dataclass
 
 import ops
 from charms.grafana_agent.v0.cos_agent import COSAgentProvider
@@ -23,6 +24,21 @@ logger = logging.getLogger(__name__)
 
 class RunEvent(ops.EventBase):
     """Event representing a periodic image builder run."""
+
+
+@dataclass
+class _Configs:
+    """Data class to hold builder configurations.
+
+    Attributes:
+        builder_config: The builder configuration state.
+        static_config: The static configurations required to build the image.
+        config_matrix: The configuration matrix for the image build.
+    """
+
+    builder_config: state.BuilderConfig
+    static_config: builder.StaticConfigs
+    config_matrix: builder.ConfigMatrix
 
 
 class GithubRunnerImageBuilderCharm(ops.CharmBase):
@@ -108,16 +124,16 @@ class GithubRunnerImageBuilderCharm(ops.CharmBase):
             cloud_config=builder_config_state.cloud_config.openstack_clouds_config
         )
         cloud_id = clouds_auth_config.get_id()
-        builder_config = state.BuilderConfig.from_charm(self)
-        config_matrix = self._get_configuration_matrix(builder_config=builder_config)
-        static_config = self._get_static_config(builder_config=builder_config)
+        configs = self._get_configs()
+        static_config = configs.static_config
         static_config.cloud_config.upload_clouds = [cloud_id]
         if cloud_images := builder.get_latest_images(
-            config_matrix=config_matrix, static_config=static_config
+            config_matrix=configs.config_matrix, static_config=static_config
         ):
             logger.info(
-                "Latest image already exists for %s. Skipping image building.",
+                "An image already exists for %s in cloud %s. Skipping image building.",
                 evt.unit.name,
+                cloud_id,
             )
 
             self.image_observer.update_image_data([cloud_images])
@@ -191,13 +207,12 @@ class GithubRunnerImageBuilderCharm(ops.CharmBase):
         logger.info(
             "Building image and uploading to %s.", (cloud_id if cloud_id else "all clouds")
         )
-        builder_config = state.BuilderConfig.from_charm(self)
-        config_matrix = self._get_configuration_matrix(builder_config=builder_config)
-        static_config = self._get_static_config(builder_config=builder_config)
+        configs = self._get_configs()
+        static_config = configs.static_config
         if cloud_id:
             static_config.cloud_config.upload_clouds = [cloud_id]
         cloud_images = builder.run(
-            config_matrix=config_matrix,
+            config_matrix=configs.config_matrix,
             static_config=static_config,
         )
         self.image_observer.update_image_data(cloud_images=cloud_images)
@@ -209,10 +224,23 @@ class GithubRunnerImageBuilderCharm(ops.CharmBase):
                     "log_type": "image_build",
                     "duration": duration,
                     "cloud_images_count": len(cloud_images),
-                    "arch": builder_config.image_config.arch,
-                    "bases": builder_config.image_config.bases,
+                    "arch": configs.builder_config.image_config.arch,
+                    "bases": configs.builder_config.image_config.bases,
                 }
             )
+        )
+
+    def _get_configs(self) -> _Configs:
+        """Get the builder configurations.
+
+        Returns:
+            The builder configurations.
+        """
+        builder_config = state.BuilderConfig.from_charm(charm=self)
+        return _Configs(
+            builder_config=builder_config,
+            static_config=self._get_static_config(builder_config=builder_config),
+            config_matrix=self._get_configuration_matrix(builder_config=builder_config),
         )
 
     def _get_configuration_matrix(
