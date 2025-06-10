ddfd6fd013f596b36e5127a77bac02c077937cc8
charlie4284 <yangsoo.yoon@canonical.com>
2025-06-10 02:23:24 +0000

    feat: log timestamp

--- app/src/github_runner_image_builder/logging.py
+++ app/src/github_runner_image_builder/logging.py
@@ -22,6 +22,8 @@ def configure(log_level: str | int) -> None:
     log_handler = logging.handlers.WatchedFileHandler(filename=LOG_FILE_PATH, encoding="utf-8")
     log_level_normalized = log_level.upper() if isinstance(log_level, str) else log_level
     log_handler.setLevel(log_level_normalized)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    log_handler.setFormatter(formatter)
     logging.basicConfig(
         level=log_level_normalized,
         handlers=(log_handler,),
