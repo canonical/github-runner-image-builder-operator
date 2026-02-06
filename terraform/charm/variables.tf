789e24208ceb873a4adc4983b904bb192f83d47e
florentianayuwono <florentiana.yuwono@canonical.com>
2026-02-06 18:16:42 +0700

    feat: upgrade terraform version

--- terraform/charm/variables.tf
+++ terraform/charm/variables.tf
@@ -25,8 +25,8 @@ variable "constraints" {
   default     = ""
 }
 
-variable "model" {
-  description = "Reference to a `juju_model`."
+variable "model_uuid" {
+  description = "Reference to a Juju model UUID."
   type        = string
   default     = ""
 }
