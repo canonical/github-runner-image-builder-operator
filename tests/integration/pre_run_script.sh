08d10b44d1abc91ef0ab832130dcc71eaed349e1
swetha1654 <swetha.swaminathan@canonical.com>
2026-02-10 11:52:34 +0530

    fix script

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 lxc list
 IP=$(lxc list -c 4 --format csv | awk '{print $1}' | head -n 1)
 echo "IP=$IP"
-sudo nft insert rule ip aproxy prerouting index 0 ip saddr != $IP return
+sudo nft insert rule ip aproxy prerouting index 0 ip saddr != "$IP" return
