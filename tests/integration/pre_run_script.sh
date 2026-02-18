4240a991d62f790a982ce2b293ad141e9f5dfe7f
swetha1654 <swetha.swaminathan@canonical.com>
2026-02-18 10:31:21 +0530

    add comment

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -5,8 +5,12 @@
 
 set -euo pipefail
 
+# charmcraft pack runs inside the LXD container and needs to go through proxy so it 
+# will be done before modifying the nftables rules.
 /snap/bin/charmcraft pack -p tests/integration/data/charm
 lxc list
 IP=$(lxc list -c 4 --format csv | awk '{print $1}' | head -n 1)
 echo "IP=$IP"
+# we want only the juju controller traffic to go through aproxy to spin up machines for the charms.
+# Any other charm related traffic should handle proxy by itself.
 sudo nft insert rule ip aproxy prerouting index 0 ip saddr != "$IP" return
