428c13671134c27dc6b5047c2daa2731b1ef90f1
yhaliaw <andrew.liaw@canonical.com>
2026-03-16 08:27:02 +0800

    Update the test script

--- app/tests/integration/helpers.py
+++ app/tests/integration/helpers.py
@@ -39,7 +39,7 @@ logger = logging.getLogger(__name__)
 
 TESTDATA_TEST_SCRIPT_URL = (
     "https://raw.githubusercontent.com/canonical/github-runner-image-builder-operator/"
-    "1d0582749df7e07d903aa72932a979de4900c48c/app/tests/integration/testdata/test_script.sh"
+    "be135aa505b37aae29aec0ab13805909c46b7903/app/tests/integration/testdata/test_script.sh"
 )
 
 
